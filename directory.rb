71e37ebf01d87da3dcd9515f9e203e48ccc881f2
Ayazg <ayaz0206@gmail.com>
2014-01-07 19:15:57 +0000

    Put students into an array.

--- directory.rb
+++ directory.rb
@@ -1,24 +1,30 @@
-student_count = 9
-# This is the list of students
+# Putting all the students into an array
+students = [
+	 "Abraham Thomas"
+	 "Andrew Krahmal"
+	 "Ayaz Goulamabasse"
+	 "Berta Gutierrez"
+	 "Biwek Shrestha"
+	 "Colin Frankish"
+	 "James Hunter"
+	 "Jorja Tracy Hung"
+	 "Kalle Malmiharju"
+	 "Makis Otman"
+	 "Megan Folsom"
+	 "Mihai-Liviu Cojocar"
+	 "Nabin Rai"
+	 "Peter Saxton"
+	 "Rolando Barbella"
+	 "Roy Gardiner"
+]
+# and now to print them
 puts "These are the students in my cohort at Makers Academy"
 puts "------------"
 
-puts "Abraham Thomas"
-puts "Andrew Krahmal"
-puts "Ayaz Goulamabasse"
-puts "Berta Gutierrez"
-puts "Biwek Shrestha"
-puts "Colin Frankish"
-puts "James Hunter"
-puts "Jorja Tracy Hung"
-puts "Kalle Malmiharju"
-puts "Makis Otman"
-puts "Megan Folsom"
-puts "Mihai-Liviu Cojocar"
-puts "Nabin Rai"
-puts "Peter Saxton"
-puts "Rolando Barbella"
-puts "Roy Gardiner"
+students.each do |student|
+	puts student
+end
+
 # Total number of students
-puts "Overall, there are #{student_count} great students"
+puts "Overall, there are #{students.length} great students"
 # This is just a side note to say that print does not add new lines.
