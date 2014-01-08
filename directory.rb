4722bda0ac6a7aea4a607623f5b2984c3aba3cc8
Ayazg <ayaz0206@gmail.com>
2014-01-08 09:13:26 +0000

    Code refactoring using methods

--- directory.rb
+++ directory.rb
@@ -17,14 +17,21 @@ students = [
 	 "Rolando Barbella"
 	 "Roy Gardiner"
 ]
-# and now to print them
-puts "These are the students in my cohort at Makers Academy"
-puts "------------"
-
-students.each do |student|
-	puts student
+def print_header
+	puts "These are the students in my cohort at Makers Academy"
+	puts "------------"
 end
 
-# Total number of students
-puts "Overall, there are #{students.length} great students"
-# This is just a side note to say that print does not add new lines.
+def print(names)
+	names.each do |student|
+		puts name
+	end
+end
+
+def print_footer(names)
+	puts "Overall, there are #{students.length} great students"
+end 
+# nothing happens until we call the methods.
+print_header
+print(students)
+print_footer(students)
