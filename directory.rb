49c8253d87aff07573f06f2a19bc28fdea633846
Ayazg <ayaz0206@gmail.com>
2014-01-14 15:55:26 +0000

    Added cohort and country information.

--- directory.rb
+++ directory.rb
@@ -1,23 +1,23 @@
 def print_header
-	puts "These are the students in my cohort at Makers Academy".center(100)
+	puts "These are the students in my cohort at Makers Academy !".center(100)
 	puts "------------".center(100)
 end
 
 def print(students)
 	students.each_with_index do |student, x|
-		puts "#{x + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+		puts "#{x + 1}. #{student[:name]} (#{student[:cohort]} cohort) from #{student[:country]}"
 	end
 end
 
 def first_letter_filter(students) 
-    puts "Choose letter:"
+    puts "Classify the names by choosing the first letter of the names:".center(100)
     letter = gets.chomp
     selected_students = students.select { |student| student[:name].chars.first == letter }
     print(selected_students)
 end
 
 def namelength(students)
-	puts "Please choose the maximum name length in characters"
+	puts "Please choose the maximum name length in characters: ".center(100)
 	size = gets.chomp.to_i
 	selected_students = students.select { |student| student[:name].length <= size}
 	print(selected_students)
@@ -32,16 +32,23 @@ def print_footer(names)
 end 
 
 def input_students
-	puts "Please enter the names of the students".center(100)
-	puts "To finish, just press enter twice".center(100)
+	puts "Please enter the names of the students:".center(100)
+	puts "To finish, just press enter twice..".center(100)
+
 	# Creating an empty array
 	students =[]
 	# Now, to ask input from user to get the first name
 	name = gets.chomp
+	
 	# While the name is not empty, repeat this code
 	while !name.empty? do
 		# Add the student hash to the array
-		students << {:name => name, :cohort => :january}
+		puts "Please enter the cohort."
+		cohort = gets.chomp
+
+		puts "Please enter the country"
+		country = gets.chomp
+		students << {:name => name, :cohort => cohort, :country => country}
 		if students.length > 1
 			puts "Now there are #{students.length} students"
 		else
